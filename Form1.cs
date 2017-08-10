5eaa51c5cde3ed9dc5507df866f285f49c1fcd1f
Andy Xu <axu@blizzard.com>
2017-08-10 14:28:35 -0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpAdbClient;
+
+namespace ADB_Pilot
+{
+	public partial class Form1 : Form
+	{
+		public Form1 ()
+		{
+			InitializeComponent();
+
+			var list = AdbClient.Instance.GetDevices().Where( d => d.State.Equals( DeviceState.Online ) );
+
+			var ds = new BindingSource() { DataSource = list };
+			listBox1.DataSource = ds;
+			listBox1.DisplayMember = "Name";
+			Refresh();
+		}
+	}
+}
